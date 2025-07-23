40ddf2bb5ccbc59217a6e7c96b8e7616dc4299a8
rokaroma <romanyjcb@gmail.com>
2025-07-23 21:27:38 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -25,9 +25,3 @@ module "sonar_instance" {
   instance_name = "sonar"
 }
 
-module "test_instance" {
-  source       = "./ec2_instance"
-  ami          = var.aws_ami  # Example AMI ID, replace with your desired AMI
-  instance_type = var.aws_type              # Example instance type, replace with your desired type
-  instance_name = "test"
-}
