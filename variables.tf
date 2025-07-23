1c6ae5cea248c124ff05f0eafef26231e3ed81f2
rokaroma <romanyjcb@gmail.com>
2025-07-23 20:59:13 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "aws_region" {
   description = "AWS region"
-  default = "us-east-2"
+  default = "us-east-1"
 }
 
 variable "aws_type" {
@@ -10,5 +10,5 @@ variable "aws_type" {
 
 variable "aws_ami" {
   description = "AWS ami"
-  default = "ami-022661f8a4a1b91cf"
+  default = "ami-0cbbe2c6a1bb2ad63"
 }
